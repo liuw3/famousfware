2eb46bd430ee00766aa0eb0ae643893435915a15
paruldev11 <parul@predikkta.com>
2018-05-18 17:55:10 +0530

    comit code for countdown

--- app/code/Magebuzz/Core/Block/System/Config/Information.php
+++ app/code/Magebuzz/Core/Block/System/Config/Information.php
@@ -36,7 +36,8 @@ class Information extends \Magento\Config\Block\System\Config\Form\Fieldset
         \Magento\Framework\Module\ModuleListInterface $moduleList,
         \Magento\Framework\Module\ModuleResource $moduleResource,
         array $data = []
-    ) {
+    )
+    {
         parent::__construct($context, $authSession, $jsHelper, $data);
         $this->_moduleList = $moduleList;
         $this->moduleResource = $moduleResource;
@@ -50,24 +51,24 @@ class Information extends \Magento\Config\Block\System\Config\Form\Fieldset
     {
         $html = $this->_getHeaderHtml($element);
 
-		$html .= $this->_getInfo();
+        $html .= $this->_getInfo();
 
-		$html .= $this->_getFooterHtml($element);
+        $html .= $this->_getFooterHtml($element);
 
-		return $html;
+        return $html;
     }
 
-	protected function _getInfo()
-	{
-		$html = '<div class="support-info">';
-		$html .= '	<h3>Support Policy</h3>';
-		$html .= '	<p>We provide 6 months free support for all of our extensions and templates. We are not responsible for any bug or issue caused of your changes to our products. To report a bug, you can easily go to <a href="http://www.magebuzz.com/support/" title="Magebuzz Support" target="_blank">our Support Page</a>, email, call or submit a ticket.</p>';
-		$html .= '	<h3>Read the blog</h3>';
-		$html .= '	<p>The <a href="http://www.magebuzz.com/blog/" target="_blank">Magebuzz Blog</a> is updated regularly with Magento tutorials, Magebuzz new products, updates, promotions... Visit <a href="http://www.magebuzz.com/blog/" target="_blank">Magebuzz Blog</a> recently to be kept updated.</p>';
-		$html .= '	<h3>Follow Us</h3>';
-		$html .= '	<div class="magebuzz-follow"><ul><li class="facebook"><a href="http://www.facebook.com/MageBuzz" title="Facebook" target="_blank"><img src="' . $this->getViewFileUrl('Magebuzz_Core::images/facebook.png') . '" alt="Facebook"/></a></li><li class="twitter"><a href="https://twitter.com/MageBuzz" title="Twitter" target="_blank"><img src="' . $this->getViewFileUrl('Magebuzz_Core::images/twitter.png') . '" alt="Twitter"/></a></li></ul></div>';
-		$html .= '</div>';
+    protected function _getInfo()
+    {
+        $html = '<div class="support-info">';
+        $html .= '	<h3>Support Policy</h3>';
+        $html .= '	<p>We provide 6 months free support for all of our extensions and templates. We are not responsible for any bug or issue caused of your changes to our products. To report a bug, you can easily go to <a href="http://www.magebuzz.com/support/" title="Magebuzz Support" target="_blank">our Support Page</a>, email, call or submit a ticket.</p>';
+        $html .= '	<h3>Read the blog</h3>';
+        $html .= '	<p>The <a href="http://www.magebuzz.com/blog/" target="_blank">Magebuzz Blog</a> is updated regularly with Magento tutorials, Magebuzz new products, updates, promotions... Visit <a href="http://www.magebuzz.com/blog/" target="_blank">Magebuzz Blog</a> recently to be kept updated.</p>';
+        $html .= '	<h3>Follow Us</h3>';
+        $html .= '	<div class="magebuzz-follow"><ul><li class="facebook"><a href="http://www.facebook.com/MageBuzz" title="Facebook" target="_blank"><img src="' . $this->getViewFileUrl('Magebuzz_Core::images/facebook.png') . '" alt="Facebook"/></a></li><li class="twitter"><a href="https://twitter.com/MageBuzz" title="Twitter" target="_blank"><img src="' . $this->getViewFileUrl('Magebuzz_Core::images/twitter.png') . '" alt="Twitter"/></a></li></ul></div>';
+        $html .= '</div>';
 
-		return $html;
-	}
+        return $html;
+    }
 }
